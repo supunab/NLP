51a7560d41c023e8edc84b106422a077c85592ec
smb564 <supun.14@cse.mrt.ac.lk>
2017-07-11 22:32:20 +0800

    added blank CRF class

---
+++ src/com/smbsoft/CRF.java
@@ -0,0 +1,19 @@
+package com.smbsoft;
+
+import java.util.ArrayList;
+
+public class CRF {
+
+
+    public void execute(){
+        ArrayList<String> states = new ArrayList<>();
+
+
+
+    }
+
+
+    private float[] getFeatures(){
+
+    }
+}
